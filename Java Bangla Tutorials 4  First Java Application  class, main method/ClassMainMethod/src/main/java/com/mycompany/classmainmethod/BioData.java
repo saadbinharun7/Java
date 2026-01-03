782629bc3f0ac673ee13da0ed4ed77fe0b2ce777
Pha raoh <pharaoh1822005@gmail.com>
2026-01-03 18:05:51 +0600

    Java Bangla Tutorials 4 : First Java Application | class, main method -done

---
+++ Java Bangla Tutorials 4  First Java Application  class, main method/ClassMainMethod/src/main/java/com/mycompany/classmainmethod/BioData.java
@@ -0,0 +1,11 @@
+package com.mycompany.classmainmethod;
+
+public class BioData {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Saad bin harun");
+        System.out.print("01710000000");
+        
+    }
+}
