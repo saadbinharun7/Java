6ceeaaedaec40887a602d94f887a35e7bced6d07
Pha raoh <pharaoh1822005@gmail.com>
2026-01-03 18:37:01 +0600

    Java Bangla Tutorials 6 : Escape sequence -done

---
+++ Java Bangla Tutorials 6  Escape sequence/EscapeSequence/src/main/java/com/mycompany/escapesequence/EscapeSequence.java
@@ -0,0 +1,13 @@
+package com.mycompany.escapesequence;
+
+public class EscapeSequence {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Saad bin harun \n01710000000");
+        System.out.println("1 \t 2");
+        System.out.println("3 \t 4");
+        System.out.println("\"Java programming\"");
+    }
+    
+}
