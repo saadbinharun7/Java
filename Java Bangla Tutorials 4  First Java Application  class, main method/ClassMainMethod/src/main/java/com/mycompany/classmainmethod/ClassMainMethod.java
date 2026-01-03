fa04dd38fa6641c9aa1c21bd0c82c101ee90998e
Pha raoh <pharaoh1822005@gmail.com>
2026-01-03 17:45:27 +0600

    Initial project upload

---
+++ Java Bangla Tutorials 4  First Java Application  class, main method/ClassMainMethod/src/main/java/com/mycompany/classmainmethod/ClassMainMethod.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.classmainmethod;
+
+/**
+ *
+ * @author phara
+ */
+public class ClassMainMethod {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
